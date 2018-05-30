1f10978ba3638234aab27ca7ab697a3ac217351e
Angel Ruiz <angel.g.r746@gmail.com>
2018-05-29 21:54:59 -0500

    Inicio de sesion con Firebase

---
+++ JAngelGRuiz/Firebase/main.js
@@ -0,0 +1,29 @@
+let btnLogin = document.getElementById("login")
+let btnLogout = document.getElementById("logout")
+
+console.log(btnLogout, btnLogin)
+
+firebase.auth().onAuthStateChanged(function(user){
+	console.log(user)
+
+	if(user){
+		console.log("hay usuario")
+	}else{
+
+	}
+})
+
+
+btnLogin.addEventListener("click", function(){
+	console.log("login")
+
+	let provider = new firebase.auth.GoogleAuthProvider();
+	provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
+
+	firebase.auth().signInWithPopup(provider)
+	.then((user) =>{
+		console.log(user)
+	}).catch((error) =>{
+		console.log(error)
+	})
+})
