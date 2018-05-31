b1e75186d86bf4e4508b9bbdd1541e4fbf9752d3
angeloLuna <angel.luna.najera@gmail.com>
2018-05-30 21:17:18 -0500

    push de datos

--- angeloLuna/firebase/main.js
+++ angeloLuna/firebase/main.js
@@ -1,21 +1,38 @@
+// Elementos de html
 let btnLogin = document.getElementById("login");
 let btnLogout = document.getElementById("logout");
+let btnGuardar = document.getElementById("guardar");
+let nombre = document.getElementById("nombre");
 
+// Referencia a la base de datos
+const ref = firebase.database().ref("usuarios");
 
+// Escuchador de sesion
 firebase.auth().onAuthStateChanged(function(user){
   console.log(user)
   if (user) {
     console.log("hay usuario")
-    showLogin()
+    showLogout()
   }else{
     console.log("no hay usuario");
+    showLogin()
   }
 })
 
-const showLogin = () =>{
-  btnLogin.style.display = "none"
+// mostrar logout
+const showLogout = () =>{
+  btnLogin.style.display = "none";
+  btnLogout.style.display = "block";
 }
 
+// ocultar logout
+const showLogin = () =>{
+  btnLogin.style.display = "block";
+  btnLogout.style.display = "none";
+}
+
+
+// iniciar sesion
 btnLogin.addEventListener("click", function(){
   console.log("login");
 
@@ -27,5 +44,45 @@ btnLogin.addEventListener("click", function(){
   }).catch((error)=>{
     console.log(error)
   })
+})
+
+
+btnLogout.addEventListener("click", ()=>{
+  firebase.auth().signOut().then(function(){
+    alert("Se ha cerrado la sesión");
+  })
+})
+
+btnGuardar.addEventListener("click", ()=>{
+  // .value para obtener el valor de un input
+  let objeto = {
+    nombre: nombre.value
+  }
+  console.log(objeto)
+
+  ref.push(objeto).then(()=>{
+    alert("se subió");
+  }).catch((error)=>{
+    console.log(error)
+  })
+
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
