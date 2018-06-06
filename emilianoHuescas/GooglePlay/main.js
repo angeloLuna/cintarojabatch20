5860c0836490c02ee49d29e282610512dd443c1d
emilianoHuescas <cehj999@gmail.com>
2018-06-05 21:39:04 -0500

    agregado de inicio de sesion

--- emilianoHuescas/GooglePlay/main.js
+++ emilianoHuescas/GooglePlay/main.js
@@ -1,16 +1,29 @@
 let btnLogin = document.getElementById("login");
+let f1 = document.getElementById("f1");
+let f2 = document.getElementById("f2");
+let f3 = document.getElementById("f3");
+let f4 = document.getElementById("f4");
+let f5 = document.getElementById("f5");
+let f6 = document.getElementById("f6");
+let f7 = document.getElementById("f7");
+let f8 = document.getElementById("f8");
+let f9 = document.getElementById("f8");
+
+// referencia a la base de datos
+const ref = firebase.database().ref("funciones");
+
 
 // Escuchador de sesion
-// firebase.auth().onAuthStateChanged(function(user){
-//   console.log(user)
-//   if (user) {
-//     console.log("hay usuario")
-//     showLogout();
-//   }else{
-//     console.log("no hay usuario")
-//     showLogin();
-//   }
-// });
+firebase.auth().onAuthStateChanged(function(user){
+  console.log(user)
+  if (user) {
+    console.log("hay usuario")
+    // showLogout();
+  }else{
+    console.log("no hay usuario")
+    // showLogin();
+  }
+});
 
 // Ìniciar sesion
 btnLogin.addEventListener("click", function() {
@@ -26,3 +39,4 @@ btnLogin.addEventListener("click", function() {
   });
 
 });
+
