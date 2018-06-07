5caffb3bb934f8686e6b01f8ea1edcb0867e4c84
Miguel Angel Franco Peza <mfranco_98@yahoo.com>
2018-06-06 21:47:32 -0500

    Se hizo un backend a mano

---
+++ Mig_Franco/backGooglePlay/index.js
@@ -0,0 +1,18 @@
+
+let express = require('express');
+let app = express();
+
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/", (request, response)=>{
+  response.send("Holi boli");
+})
+
+
+app.listen(3000, ()=>{
+  console.log("Estoy en el puerto 3000")
+});
